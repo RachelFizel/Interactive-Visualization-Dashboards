fea271b8efa7d6bfc987370548fdad720998bf3b
Rachel <rachel.fizel@gmail.com>
2021-04-27 13:09:27 -0500

    populated drop down list

--- static/js/app.js
+++ static/js/app.js
@@ -8,9 +8,19 @@ function initDashboard(){
     //populate the dropdown
     var selector = d3.select("#selDataset");
 
-    d3.json("data/samples.json").then(function(data){
+    d3.json("data/samples.json").then(data => {
         console.log(data);
+
+        var sampleNames = data.names;
+
+        sampleNames.forEach(sampleId => {
+            selector.append("option")
+                .text(sampleId)
+                .property("value", sampleId);
+        });
     });
+
+
 
     //update the bar graph
 
